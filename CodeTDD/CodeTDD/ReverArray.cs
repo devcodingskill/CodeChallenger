4bd82667b0d1c4835e6c3a94ab9d6eee746f6d9f
Tan <decodingskill@gmail.com>
2021-05-02 11:28:57 +1000

    Add Revert class

---
+++ CodeTDD/CodeTDD/ReverArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace CodeTDD
+{
+    public class ReverArray
+    {
+        public ReverArray()
+        {
+            
+        }
+        public int[] ReverIntArray2(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result[i] = nums[nums.Length - (i + 1)];
+
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CodeTDD/UnitTest/UnitTestReverArray.cs
@@ -0,0 +1,28 @@
+﻿using CodeTDD;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    public class UnitTestReverArray
+    {
+        ReverArray reverInt;
+          
+            [SetUp]
+            public void Setup()
+            {
+               reverInt = new ReverArray();
+            }
+
+            [Test]
+            [TestCase(new int[] { 1, 2, 5 }, new int[] { 5, 2, 1 })]
+            [TestCase(new int[] { 3, -3, 5 },new int[] { 5, -3,3 })]
+            [TestCase(new int[] { 3, 2, 3 }, new int[] { 3, 2,3 })]
+            [TestCase(new int[] { 1, 2, 3 },  new int[] { 3, 2,1 })]
+            public void TestingReverArrayInt(int[] nums, int[] expected)
+            {
+                   var result= reverInt.ReverIntArray2(nums);
+                    Assert.AreEqual(expected, result);
+            }
+        }
+    
+}
