7dc0c13742ccf1466f4e792056dbadd2f496e26c
Tan <decodingskill@gmail.com>
2021-05-03 04:15:15 +1000

    add int revert'

--- CodeTDD/CodeTDD/ReverArray.cs
+++ CodeTDD/CodeTDD/ReverArray.cs
@@ -20,5 +20,48 @@ namespace CodeTDD
 
             return result;
         }
+        public string[] RevertStringArray2(string[] words)
+        {
+
+            string[] result = new string[words.Length];
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                result[i] = words[words.Length - (i + 1)];
+
+
+            }
+            return result;
+        }
+        public int RevertInt2(int nums)
+        {
+            
+            int result = 0;
+            char[] numArray = nums.ToString().ToCharArray();
+            char[] temp = new char[numArray.Length];
+
+            if (numArray[0].Equals('-') || numArray[0].Equals('+'))
+            {
+                temp[0] = numArray[0];
+                for (int i = 1; i < numArray.Length; i++)
+                {
+                    temp[i] = numArray[numArray.Length - (i)];
+                }
+            }
+            else
+            {
+
+                for (int i = 0; i < numArray.Length; i++)
+                {
+
+                    temp[i] = numArray[numArray.Length - (i + 1)];
+                }
+            }
+           // var t = new string(temp);
+            result = int.Parse(new string(temp));
+            return result;
+
+        }
+        
     }
 }
--- CodeTDD/UnitTest/UnitTestReverArray.cs
+++ CodeTDD/UnitTest/UnitTestReverArray.cs
@@ -23,6 +23,30 @@ namespace UnitTest
                    var result= reverInt.ReverIntArray2(nums);
                     Assert.AreEqual(expected, result);
             }
+
+
+        [Test]
+        [TestCase(new string[] { "me", "hit", "you" }, new string[] { "you", "hit", "me" })]
+        //[TestCase(new string[] { 3, -3, 5 }, new string[] { 5, -3, 3 })]
+        //[TestCase(new string[] { 3, 2, 3 }, new string[] { 3, 2, 3 })]
+        //[TestCase(new string[] { 1, 2, 3 }, new string[] { 3, 2, 1 })]
+        public void TestingReverStringInt(string[] words, string[] expected)
+        {
+            var result = reverInt.RevertStringArray2(words);
+            Assert.AreEqual(expected, result);
         }
+
+        [Test]
+        [TestCase( 123,321)]
+        [TestCase(-123, -321)]
+        //[TestCase(new string[] { 3, -3, 5 }, new string[] { 5, -3, 3 })]
+        //[TestCase(new string[] { 3, 2, 3 }, new string[] { 3, 2, 3 })]
+        //[TestCase(new string[] { 1, 2, 3 }, new string[] { 3, 2, 1 })]
+        public void TestingReverInt(int nums, int expected)
+        {
+            var result = reverInt.RevertInt2(nums);
+            Assert.AreEqual(expected, result);
+        }
+    }
     
 }
